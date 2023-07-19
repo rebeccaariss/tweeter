a5f7b92a83f8e1ff23b794270625c6d65a3b379b
Rebecca Ariss <rebeccamarionruth@gmail.com>
2023-07-19 15:26:42 -0400

    Wrap client.js code in jQuery's document ready function; fix selector for '.tweet-container' to render tweet on page

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -4,68 +4,68 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 
-const tweetData = {
-  "user": {
-    "name": "Newton",
-    "avatars": "https://i.imgur.com/73hZDYK.png",
-    "handle": "@SirIsaac"
-  },
-  "content": {
-    "text": "If I have seen further it is by standing on the shoulders of giants"
-  },
-  "created_at": 1461116232227
-};
+$(document).ready(function() {
+  const tweetData = {
+    "user": {
+      "name": "Newton",
+      "avatars": "https://i.imgur.com/73hZDYK.png",
+      "handle": "@SirIsaac"
+    },
+    "content": {
+      "text": "If I have seen further it is by standing on the shoulders of giants"
+    },
+    "created_at": 1461116232227
+  };
 
-const renderTweets = function(tweets) {
-  // loops through tweets
-  // calls createTweetElement for each tweet
-  // takes return value and appends it to the tweets container
+  const renderTweets = function(tweets) {
+    // loops through tweets
+    // calls createTweetElement for each tweet
+    // takes return value and appends it to the tweets container
 
-  // will take in an array of tweet objects and append each one to the #tweet-container
-  // will need to leverage crewteTweetElement function to do so (below)
-  // pass tweet into it. jQuery object returned out of that function will be appended to container.
-};
+    // will take in an array of tweet objects and append each one to the #tweet-container
+    // will need to leverage crewteTweetElement function to do so (below)
+    // pass tweet into it. jQuery object returned out of that function will be appended to container.
+  };
 
-const createTweetElement = function(tweet) {
-  // initial example:
-  // let $tweet = $(`<article class="tweet">Hello world</article>`);
+  const createTweetElement = function(tweet) {
+    let $tweet =
+      $(`<article class="tweet">
+        <header>
+          <div class="author-info">
+            <img src=${tweet.user.avatars} alt="author profile picture"/>
+            <p class="author-name">${tweet.user.name}</p>
+          </div>
+          <p class="author-handle">${tweet.user.handle}</p>
+        </header>
+        <p class="tweet-text">${tweet.content.text}</p>
+        <footer>
+          <h6>${tweet.created_at}</h6>
+          <div class="icons">
+            <a href="" class="tweet-action-icon">
+              <i class="fa-solid fa-flag"></i>
+            </a>
+            <a href="" class="tweet-action-icon">
+              <i class="fa-solid fa-retweet"></i>
+            </a>
+            <a href="" class="tweet-action-icon">
+              <i class="fa-solid fa-heart"></i>
+            </a>
+          </div>
+        </footer>
+      </article>`);
 
-  let $tweet =
-    $(`<article class="tweet">
-      <header>
-        <div class="author-info">
-          <img src=${tweet.user.avatars} alt="author profile picture"/>
-          <p class="author-name">${tweet.user.name}</p>
-        </div>
-        <p class="author-handle">${tweet.user.handle}</p>
-      </header>
-      <p class="tweet-text">${tweet.content.text}</p>
-      <footer>
-        <h6>${tweet.created_at}</h6>
-        <div class="icons">
-          <a href="" class="tweet-action-icon">
-            <i class="fa-solid fa-flag"></i>
-          </a>
-          <a href="" class="tweet-action-icon">
-            <i class="fa-solid fa-retweet"></i>
-          </a>
-          <a href="" class="tweet-action-icon">
-            <i class="fa-solid fa-heart"></i>
-          </a>
-        </div>
-      </footer>
-    </article>`);
+    return $tweet;
+  };
 
-  return $tweet;
-};
+  // renderTweets(data);
 
-renderTweets(data);
+  // ––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––
 
-// ––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––
+  // Test / driver code (temporary). Eventually will get this from the server.
+  const $tweet = createTweetElement(tweetData);
 
-// Test / driver code (temporary). Eventually will get this from the server.
-const $tweet = createTweetElement(tweetData);
+  // Test / driver code (temporary)
+  console.log($tweet); // to see what it looks like
+  $('.tweet-container').append($tweet); // to add it to the page so we can make sure it's got all the right elements, classes, etc.
 
-// Test / driver code (temporary)
-console.log($tweet); // to see what it looks like
-$('#tweets-container').append($tweet); // to add it to the page so we can make sure it's got all the right elements, classes, etc.
+});
