2a513fd78aa8fbd631d0466fbcfed35622159106
Rebecca Ariss <rebeccamarionruth@gmail.com>
2023-07-19 23:25:54 -0400

    Implement GET with AJAX for fetching  tweets (loadTweets function)

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -5,30 +5,7 @@
  */
 
 $(document).ready(function() {
-  const data = [
-    {
-      "user": {
-        "name": "Newton",
-        "avatars": "https://i.imgur.com/73hZDYK.png"
-        ,
-        "handle": "@SirIsaac"
-      },
-      "content": {
-        "text": "If I have seen further it is by standing on the shoulders of giants"
-      },
-      "created_at": 1461116232227
-    },
-    {
-      "user": {
-        "name": "Descartes",
-        "avatars": "https://i.imgur.com/nlhLi3I.png",
-        "handle": "@rd" },
-      "content": {
-        "text": "Je pense , donc je suis"
-      },
-      "created_at": 1461113959088
-    }
-  ];
+  const $form = $("#tweet-post-form");
 
   const createTweetElement = function(tweet) {
     let $tweet =
@@ -65,9 +42,18 @@ $(document).ready(function() {
     }
   };
 
-  renderTweets(data);
-
-  const $form = $("#post-tweet");
+  const loadTweets = function() {
+    $.ajax({
+      method: "GET",
+      url: "/tweets",
+      success: (tweets) => {
+        renderTweets(tweets);
+      },
+      fail: (error) => {
+        console.log("error: ", error);
+      }
+    });
+  };
 
   $form.on("submit", function(event) {
     event.preventDefault(); // event.preventDefault prevents the default
@@ -77,15 +63,18 @@ $(document).ready(function() {
     const $tweet = $(this).serialize();
 
     $.ajax({
-      type: "POST",
+      method: "POST",
       url: "/tweets",
       data: $tweet,
-      success: function() {
-        console.log("success!");
+      success: () => {
+        loadTweets();
       },
-      fail: function() {
-        console.log("error");
+      fail: (error) => {
+        console.log("error: ", error);
       }
     });
   });
+
+  loadTweets();
+
 });
